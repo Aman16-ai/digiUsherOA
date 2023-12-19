c771467dcc77a44b875806319b8c897237897824
aman <asaxena7531@gmail.com>
2023-12-19 22:15:14 +0530

    small fix

--- analysis/services/ReportService.py
+++ analysis/services/ReportService.py
@@ -14,5 +14,5 @@ class ReportService:
     def generate(self):
         day_avg_metrics = self.metricsService.get_average_mertics(startDate=self.start_date_day,endDate=self.present_date)
 
-        month_avg_metrics = day_avg_metrics = self.metricsService.get_average_mertics(startDate=self.start_date_month,endDate=self.present_date)
+        month_avg_metrics = self.metricsService.get_average_mertics(startDate=self.start_date_month,endDate=self.present_date)
         return {'day':day_avg_metrics,'month':month_avg_metrics}
